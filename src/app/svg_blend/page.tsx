7eaea1365a2b9bc6b0968ed20c083f6c60b74738
Sazzad Hossain <tanimsazzad@gmail.com>
2023-07-31 16:58:03 +0600

    added new pattern svg

--- src/app/svg_blend/page.tsx
+++ src/app/svg_blend/page.tsx
@@ -1,33 +1,41 @@
 import Image from 'next/image'
-import bg2 from '@svg/stacked-waves.svg'
-import capture from '@png/capture2.png'
+import bg2 from '@svg/blurry-gradient.svg'
+import pattern from '@svg/pattern3.svg'
 
 export default function Home() {
   return (
-    <main className='py-10'>
+    <main className='h-screen bg-my_pattern bg-cover py-10'>
       {/* background */}
+
       <Image
         alt='background'
         src={bg2}
-        className='absolute top-0 -z-10 h-full w-full object-cover'
+        className='absolute top-0 h-full w-full object-cover mix-blend-hue'
       />
       <div className='prose mx-auto px-2 text-white'>
-        <h1 className='py-10 text-center font-mono capitalize text-white lg:whitespace-nowrap'>
+        <h1 className='py-10 text-center font-mono lowercase text-white first-letter:capitalize lg:whitespace-nowrap'>
           Single svg as background
         </h1>
         <div>
           <ol className='list-decimal marker:text-white'>
-            <li>Add svg in nextjs image component.</li>
-            <li>
+            <li className='lowercase first-letter:capitalize'>
+              top svg pattern should be used like SVG PATTERN method
+            </li>
+           
+            <li className='lowercase first-letter:capitalize'>
+              Add svg in nextjs image component.
+            </li>
+            <li className='lowercase first-letter:capitalize'>
               In image component add negative z index to make it background
             </li>
-            <li>
+            <li className='lowercase first-letter:capitalize'>
               In image component use object-cover, object-contain etc object
               property.
             </li>
-            <li>Give image component widht and height to use object-cover</li>
+            <li className='lowercase first-letter:capitalize'>
+              Give image component widht and height to use object-cover
+            </li>
           </ol>
-          <Image alt='image component' src={capture} />
         </div>
         <pre className='text-xs'>
           {`<Image
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Navigation() {
   return (
-    <div className='fixed flex w-full justify-center gap-10 bg-white/60 py-5 capitalize backdrop-blur-sm '>
+    <div className='fixed flex w-full justify-center gap-10 bg-white/60 py-5 capitalize backdrop-blur-sm z-30'>
       <Link href={'/svg_pattern'}>svg pattern</Link>
       <Link href={'/svg_full'}>svg full</Link>
       <Link href={'/png_full'}>png full</Link>
