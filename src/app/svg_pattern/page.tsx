481a62f7507ba0d1b20240b6cda89d5715d0545c
Sazzad Hossain <tanimsazzad@gmail.com>
2023-07-31 16:13:34 +0600

    added new page named png full

--- src/app/svg_pattern/page.tsx
+++ src/app/svg_pattern/page.tsx
@@ -3,9 +3,9 @@ import capture1 from '@png/capture.png'
 
 export default function Home() {
   return (
-    <main className='h-screen  w-screen bg-my_bg bg-contain py-10'>
-      <div className='prose mx-auto text-white'>
-        <h1 className='mx-auto py-10 font-mono capitalize text-white lg:whitespace-nowrap'>
+    <main className='h-screen w-screen bg-my_bg bg-contain py-10'>
+      <div className='prose mx-auto px-2  text-white'>
+        <h1 className='mx-auto py-10 text-center font-mono capitalize text-white lg:whitespace-nowrap'>
           Svg pattern as background
         </h1>
         <div>
@@ -25,6 +25,7 @@ export default function Home() {
             <li>Add bg-my_bg in className. </li>
           </ol>
         </div>
+        <pre>{`  <main className='h-screen w-screen bg-my_bg bg-contain py-10'>`}</pre>
       </div>
     </main>
   )
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,6 +6,7 @@ export default function Navigation() {
     <div className='fixed flex w-full justify-center gap-20 bg-white/60 py-5 capitalize backdrop-blur-sm '>
       <Link href={'/svg_pattern'}>svg pattern</Link>
       <Link href={'/svg_full'}>svg full</Link>
+      <Link href={'/png_full'}>png full</Link>
     </div>
   )
 }
