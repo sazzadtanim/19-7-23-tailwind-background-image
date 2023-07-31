72f74f4601b8e4da617a6384dd15fbf2eb0ac647
Sazzad Hossain <tanimsazzad@gmail.com>
2023-07-31 16:26:54 +0600

    added new svg blend page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 
 export default function Navigation() {
   return (
-    <div className='fixed flex w-full justify-center gap-20 bg-white/60 py-5 capitalize backdrop-blur-sm '>
+    <div className='fixed flex w-full justify-center gap-10 bg-white/60 py-5 capitalize backdrop-blur-sm '>
       <Link href={'/svg_pattern'}>svg pattern</Link>
       <Link href={'/svg_full'}>svg full</Link>
       <Link href={'/png_full'}>png full</Link>
+      <Link href={'/svg_blend'}>svg blend</Link>
     </div>
   )
 }
