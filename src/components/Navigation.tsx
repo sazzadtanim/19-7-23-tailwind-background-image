fe976e93803649e8ce3519b46e9e50f599cdf290
Sazzad Hossain <tanimsazzad@gmail.com>
2023-07-31 12:29:18 +0600

    added new page in navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,8 +3,9 @@ import React from "react";
 
 export default function Navigation() {
   return (
-    <div className="fixed bg-white w-full px-20 py-2">
+    <div className="fixed bg-white w-full px-20 py-2 flex gap-20">
       <Link href={"/svg_pattern"}>svg pattern</Link>
+      <Link href={"/svg_full"}>svg full</Link>
     </div>
   );
 }
