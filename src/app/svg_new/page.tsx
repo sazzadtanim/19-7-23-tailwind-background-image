342af823a9195fcd1684bc94b8763f7e5b0776fe
Sazzad Hossain <tanimsazzad@gmail.com>
2023-08-01 15:47:10 +0600

    add css gradient as background

--- src/app/svg_new/page.tsx
+++ src/app/svg_new/page.tsx
@@ -1,29 +1,25 @@
 import Image from 'next/image'
-import capture1 from '@png/capture.png'
+import capture1 from '@png/capture3.png'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Svg pattern as background',
+  title: 'css gradient as background',
   description: 'Nextjs tailwindcss bacground image implementations',
   creator: 'SazzadTanim',
 }
 
 export default function Home() {
   return (
-    <main className='h-screen w-screen bg-my_bg bg-contain py-10'>
+    <main className='h-screen w-screen bg-radial_gradient bg-contain py-10'>
       <div className='prose mx-auto px-2  text-white'>
         <h1 className='mx-auto py-10 text-center font-mono lowercase text-white first-letter:capitalize lg:whitespace-nowrap'>
-          Svg pattern as background
+          css gradient as background
         </h1>
         <div>
           <ol className='marker:text-white'>
             <li className='lowercase first-letter:capitalize'>
               <span>
-                Add svg in tailwind.config.js as background image.
-                <span className='text-red-500'>
-                  {' '}
-                  Be careful about the image path.
-                </span>
+                Add radial gradients in tailwind.config.js as background image.
               </span>
               <Image alt='' src={capture1} />
             </li>
@@ -37,7 +33,7 @@ export default function Home() {
           </ol>
         </div>
         <pre className='text-xs'>
-          {`<main className='h-screen w-screen bg-radial_gradient bg-contain py-10'>`}
+          {`<main className='h-screen w-screen bg-my_bg bg-contain py-10'>`}
         </pre>
       </div>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,15 @@ module.exports = {
       backgroundImage: {
         my_bg: `url("/svg/svg_pattern.svg")`,
         my_pattern: `url("/svg/pattern3.svg")`,
+        radial_gradient: `
+        radial-gradient(at 59% 51%, hsla(291,78%,77%,1) 0px, transparent 50%),
+        radial-gradient(at 80% 43%, hsla(231,85%,68%,1) 0px, transparent 50%),
+        radial-gradient(at 58% 63%, hsla(340,98%,68%,1) 0px, transparent 50%),
+        radial-gradient(at 83% 57%, hsla(210,67%,62%,1) 0px, transparent 50%),
+        radial-gradient(at 75% 25%, hsla(265,74%,70%,1) 0px, transparent 50%),
+        radial-gradient(at 4% 71%, hsla(220,64%,66%,1) 0px, transparent 50%),
+        radial-gradient(at 57% 5%, hsla(176,96%,71%,1) 0px, transparent 50%),
+        radial-gradient(at 14% 52%, hsla(358,65%,72%,1) 0px, transparent 50%)`,
       },
     },
   },
